3c7453349007b477a20bd360469bb04e997b0fe8
unknown <levinhhuy113@gmail.com>
2024-05-13 22:48:57 +0700

    add file

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bai1prj
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            Console.Write("Nhập số lượng phần tử của mảng: ");
+            int[] arr = new int[n];
+            for (int i = 0; i < n; i++) ;
+            {
+                Console.Write
+            }
+        }
+    }
+}
